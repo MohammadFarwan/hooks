be620ecd1bba70223c2d85e398f60579dc338562
MohammadFarwan <alfrwan58@gmail.com>
2023-06-26 13:44:49 +0300

    useRef uploded

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css'
 import Effect from './hooks/Effect/Effect'
 import { EffectExercise } from './hooks/Effect/EffectExercise'
+import { Effecttest } from './hooks/Effect/Effecttest'
+import { Ref } from './hooks/Ref/Ref'
 import State from './hooks/State/State'
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
     <>
       {/* <State /> */}
       {/* <Effect/> */}
-      <EffectExercise />
+      {/* <EffectExercise /> */}
+      {/* <Effecttest /> */}
+      <Ref/>
     </>
   )
 }
